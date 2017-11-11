32df909a0e8e2e2304bac59c8f241f8c91cb2a52
Midhet <djulovic_m@hotmail.com>
2017-11-11 20:02:18 +0100

    init commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const hbs = require("hbs");
+const fs = require("fs");
+
+hbs.registerPartials(path.join(__dirname, "views", "partials"));
+
+app.set("view engine", "hbs");
+
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt", text => {
+  return text.toUpperCase();
+});
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile("server.log", log + "\n", err => {
+    if (err) {
+      console.log("Unable to append to server.log");
+    }
+  });
+  next();
+});
+/*app.use((req, res, next) => {
+  res.render("maintenance.hbs");
+});
+*/
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("home.hbs", {
+    pageTitle: "Home Page",
+    welcomeMessage: "Welcome to root page"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.hbs", {
+    pageTitle: "About Page"
+  });
+});
+
+app.get("/bad", (req, res) => {
+  res.json({ error: "Unable to  handle request" });
+});
+
+app.listen(3000, () => console.log("Listening on port 3000"));
